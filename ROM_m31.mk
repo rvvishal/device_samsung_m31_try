319ea36a5eeafb3fe94bfdcb7906f14546c1bfc0
LMAO-armv8 <82850873+LMAO-armv8@users.noreply.github.com>
2021-07-05 11:18:59 +0530

    Update ROM_m31.mk

--- ROM_m31.mk
+++ ROM_m31.mk
@@ -15,3 +15,19 @@
 #
 
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/m31/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common ROM stuff
+ifeq ($(ROM), lineage)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+else ifeq ($(ROM_TYPE), lineage)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+else 
+$(call inherit-product, vendor/$(ROM)/config/common.mk)
+endif
