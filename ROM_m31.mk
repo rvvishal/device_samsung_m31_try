74b4d30b63aebfc8aeddd2a27188bb53a031c4d3
LMAO-armv8 <82850873+LMAO-armv8@users.noreply.github.com>
2021-07-05 11:31:43 +0530

    Update ROM_m31.mk

--- ROM_m31.mk
+++ ROM_m31.mk
@@ -31,3 +31,31 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 else 
 $(call inherit-product, vendor/$(ROM)/config/common.mk)
 endif
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m31
+PRODUCT_NAME := $(CODENAME)_m31
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M315F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Fingerprint
+ifeq ($(ANDROID_VERSION), 10)
+BUILD_FINGERPRINT := samsung/m31nsxx/m31:10/QP1A.190711.020/M315FXXU2ATJ9:user/release-keys
+else
+BUILD_FINGERPRINT := samsung/m31nsxx/m31:11/RP1A.200720.012/M315FXXU2BUD1:user/release-keys
+
+PRIVATE_BUILD_DESC_UNI= ifeq ($(ANDROID_VERSION), 10)
+                        PRIVATE_BUILD_DESC="m31nsxx-user 10 QP1A.190711.020 M315FXXU2ATJ9 release-keys"
+                        else
+                        PRIVATE_BUILD_DESC="m31nsxx-user 11 RP1A.200720.012 M315FXXU2BUD1 release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRODUCT_NAME=m31nsxx \
+        $(PRIVATE_BUILD_DESC_UNI)
+        
