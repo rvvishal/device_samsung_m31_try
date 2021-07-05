36f194690b6136de6e8f75981e6486a3e8b09256
LMAO-armv8 <82850873+LMAO-armv8@users.noreply.github.com>
2021-07-05 16:09:12 +0530

    Create setup-makefiles.sh

---
+++ setup-makefiles.sh
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+# Required!
+export DEVICE=m31
+
+ifeq ($(common_dir), universal9610-common)
+export DEVICE_COMMON=universal9610-common
+          universal9610-common 
+          universal9611-common
+export VENDOR=samsung
+
+export DEVICE_BRINGUP_YEAR=2020
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
