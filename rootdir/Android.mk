d6c3378eb10b2d9146dcd344765d1e41c539c6ad
LMAO-armv8 <82850873+LMAO-armv8@users.noreply.github.com>
2021-08-07 21:47:16 +0530

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -26,10 +26,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.exynos9611
+LOCAL_MODULE       := fstab.exynos9610
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.exynos9611
+LOCAL_SRC_FILES    := etc/fstab.exynos9610
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
